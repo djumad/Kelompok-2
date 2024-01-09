fdffc0bed71330772d57cff2bd2d495745f2e4f7
djumad <42491296+djumad@users.noreply.github.com>
2024-01-09 09:33:02 +0700

    Update stream-jantung.py

--- stream-jantung.py
+++ stream-jantung.py
@@ -2,6 +2,6 @@ import pickle
 import numpy as np
 import streamlit as st
 
-model = pickle.load(open('penyakit_jantung.sav' , 'rb'))
+model = pickle.load(open('PENYAKIT_JANTUNG.sav' , 'rb'))
 
 st.title('Prediksi Penyakit Jantung')
